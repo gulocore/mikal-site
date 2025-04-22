cb4cb33555c1e481d3afe0e505a211eabc2b6b02
Mikal Svendsen <mikal.svensdsen@postnord.com>
2025-04-22 14:39:21 +0200

    add css bundler

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,53 @@
 module.exports = async function(eleventyConfig) {
+
 const { EleventyHtmlBasePlugin } = await import("@11ty/eleventy");	
-eleventyConfig.addPlugin(EleventyHtmlBasePlugin);	
-eleventyConfig.addPassthroughCopy({ "src/media": "img" });
-eleventyConfig.addPassthroughCopy({ "src/fonts": "css/fonts" });
-}
+
+ const autoprefixer = require('autoprefixer');
+
+ const cssnano = require('cssnano');
+
+ const fs = require('fs');
+
+ const postcss = require('postcss');
+
+ const postcssImport = require('postcss-import');
+
+eleventyConfig.addWatchTarget("./src/css/**/*.css");
+eleventyConfig.addWatchTarget("./src/assets/**/*");
+
+eleventyConfig.addPassthroughCopy({ "./src/assets/images": "img" });
+eleventyConfig.addPassthroughCopy({ "./src/assets/fonts": "css/fonts" });
+
+ eleventyConfig.on('eleventy.before', async () => {
+
+   // PostCSS processing
+
+   const cssSourceFile = './src/css/main.css';
+
+   const cssDestinationFile = './_site/css/main.css';
+
+   fs.readFile(cssSourceFile, (err, css) => {
+
+     postcss([
+
+     postcssImport,
+
+     autoprefixer,
+
+     cssnano,
+
+     ])
+
+     .process(css, { from: cssSourceFile, to: cssDestinationFile })
+
+     .then(result => {
+
+       fs.writeFile(cssDestinationFile, result.css, () => true)
+
+     });
+
+   });
+ });
 
 module.exports.config = {
   dir: {
@@ -12,6 +56,7 @@ module.exports.config = {
     includes: "../includes",
     data: "../data",
   },
-  pathPrefix: "/github-repo/",
+//  pathPrefix: "/github-repo/",
 };
+}
 
